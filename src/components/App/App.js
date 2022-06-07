46124f249fe0b60d98b7a5482b9f95cf9779b8e3
KingBoolean89 <aking893.ak@gmail.com>
2022-06-07 15:19:08 -0400

    Fetch data, calculate and render summary details

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,30 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Tabs from '../Tabs/Tabs'
 
+
+
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch('https://beamtech.github.io/boxing-kata-js/perks.json')
+        .then((response) => response.json())
+        .then((result) =>{ setData(result); console.log(result)})
+        .catch((error) => console.log('An error occurred', error));
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">  
       <h1 className='appTitle'>
         Shipping
       </h1>
-      <Tabs />
+      <Tabs data={data} />
     </div>
   );
 }
--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,54 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Tabs.css'
 
-function Tabs() {
+
+
+const Tabs = ({data}) => {
     const [toggleState, setToggleState] = useState(1);
-  
+    const [memberCount, setMemberCount] = useState("");
+    const [starterCount, setStarterCount] = useState("");
+    const [refillCount, setRefillCount] = useState("")
+
+    useEffect(() => {
+      setMemberCount(data);
+      calcStarterBoxes(data);
+      calcRefillBoxes(data);
+    }, [data])
+    
     const toggleTab = (index) => {
       setToggleState(index);
     };
+
+    const calcStarterBoxes = (data) => {
+        const quotient = Math.floor(data?.length/2);
+        const remainder = data?.length % 2;
+        const totalStarters = quotient + remainder;
+        setStarterCount(totalStarters);
+    };
+
+    const calcRefillBoxes = (data) => {
+        const quotient = Math.floor(data?.length/4);
+        const remainder = data?.length % 4;
+        const totalRefills = quotient + remainder;
+        setRefillCount(totalRefills);
+    };
+
   
     return (
+
       <div className="container">
         <div className="bloc-tabs">
           <button
             className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(1)}
-          >
+            onClick={() => toggleTab(1)}>
             Starter Boxes
           </button>
           <button
             className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(2)}
-          >
+            onClick={() => toggleTab(2)}>
             Refill Boxes
-          </button>
-          
+          </button> 
         </div>
   
         <div className="content-tabs">
-          <div
-            className={toggleState === 1 ? "content  active-content" : "content"}
-          >
-            <h2>Summary:</h2>
-            <p>Starter Boxes:</p>
-            <p>Brushes:</p>
-            <p>Replacement Heads:</p>
-            
-           
+          <div className={toggleState === 1 ? "content  active-content" : "content"}>
+            <h2>Summary: </h2>
+            <p>Starter Boxes: {data && starterCount ? starterCount : null} </p>
+            <p>Brushes: {data && memberCount ? memberCount.length : null}</p>
+            <p>Replacement Heads: {data && memberCount ? memberCount.length : null}</p>
           </div>
   
-          <div
-            className={toggleState === 2 ? "content  active-content" : "content"}
-          >
+          <div className={toggleState === 2 ? "content  active-content" : "content"}>
             <h2>Summary:</h2>
-            <p>Refill Boxes:</p>
-            <p>Replacement Heads:</p>
-            
-            
+            <p>Refill Boxes: {data && refillCount ? refillCount : null}</p>
+            <p>Replacement Heads: {data && memberCount ? memberCount.length : null}</p>  
           </div>
-  
-         
         </div>
       </div>
     );
